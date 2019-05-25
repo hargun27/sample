dff7b149e39ccb3b88ab6f19a63cdf65f1e46e35
from the class mac <hargunmarya2726@gmail.com>
2019-05-24 21:30:25 -0400

    [hargun] functions on string

--- src/main/java/com/hargun/ExampleMain.java
+++ src/main/java/com/hargun/ExampleMain.java
@@ -4,5 +4,61 @@ public class ExampleMain {
     public static void main(String[] args)
     {
 
+ int x[]=new int[5];
+ int y[]={10,20,30,40,50,60,70,80,90,100};
+ x[0]=100;
+ x[1]=200;
+ x[2]=300;
+ x[3]=400;
+ x[4]=600;
+
+ String s4="lambton college";
+ char result=s4.charAt(9);
+        System.out.println(result);
+        boolean contains=s4.contains("test");
+        System.out.println(contains);
+        boolean endsWith=s4.endsWith("ing");
+        System.out.println(endsWith);
+
+
+
+
+
+
+/* for(int i=0;i<x.length;i++)
+        {
+            System.out.println("x[]: " +x[i]);
+        }
+        for(int i=0;i<y.length;i++)
+        {
+            System.out.println("y[]: " +y[i]);
+        }
+ExampleMain main=new ExampleMain();
+int s=main.SumOfArray(x);
+System.out.println("sum of array is" +s);
+int z[]=main.
     }
+    public int SumOfArray(int a[])
+
+    {
+        int sum=0;
+        for(int x:a)
+        {
+            sum=sum+x;
+        }
+        return sum;
+    }
+    public int[] ReverseOfArray(int[] arr)
+    {
+        int rev[]=new int[arr.length];
+        int rev_index=0;
+        for(int i:arr)
+        {
+            rev[rev_index]=
+        }*/
+
+
+
+    }
+
 }
